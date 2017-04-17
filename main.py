445213131afad5213a6e7faf257689ab07b4001c
bbrownrichardson <bbrownrichardson19@wooster.edu>
2017-04-17 16:35:58 -0400

    Add ScreentoPost and a few frontend changes

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import FallOutTransition
 from PrivateMessenger import PrivateMessenger
 from PostScreen import PostScreen
+from ScreenToPost import ScreenToPost
 from mock_interface import MockServerInterface
 
 kv = Builder.load_file('my.kv')
 
-ClientApp = App.get_running_app().server_interface
+#ClientApp = App.get_running_app().server_interface
 
 
 class ScreenApp(App):
+
     def build(self):
-        self.server_interface = MockServerInterface()
+        # self.server_interface = MockServerInterface()
         self.manager = ScreenManager(transition=FallOutTransition())
-        self.manager.add_widget(PrivateMessenger(name='PrivateMessenger'))
         self.manager.add_widget(PostScreen(name='PostScreen'))
+        self.manager.add_widget(PrivateMessenger(name='PrivateMessenger'))
+        self.manager.add_widget(ScreenToPost(name='ScreenToPost'))
+
 
         return self.manager
 
